241d8a278f2bedbcb05fa9a53f322e7e1b03f58d
patrick <patrick.tyler@apparray.biz>
2021-12-05 12:50:23 +0900

    Add check() method test. Wip.

--- src/main/checker/jmty.py
+++ src/main/checker/jmty.py
@@ -5,10 +5,6 @@ import logging
 
 logger = logging.getLogger(__name__)
 
-search_obj = {
-    "search_term": "road bike"
-}
-
 
 def _extract_price(raw_price_string):
     expr = re.compile("\d+,?\d*")
@@ -33,7 +29,7 @@ def _page_contains_finished_ads(listed_items):
     return True in list(map(lambda x: x['is_finished'], listed_items))
 
 
-def is_sale_finished(url):
+def _is_sale_finished(url):
     resp = requests.get(url)
     if resp.status_code != 200:
         raise Exception
@@ -47,39 +43,44 @@ def is_sale_finished(url):
     return "終了いたしました" in p_tags.contents[2]
 
 
-def get_search_html_response(page_num=1):
+def _get_search_html_response(search_term, page_num=1):
     resp = requests.get(
         f'https://jmty.jp/all/sale/p-{page_num}',
-        params={"keyword": search_obj["search_term"]}
+        params={"keyword": search_term}
     )
     if resp.status_code != 200:
-        raise Exception
+        raise Exception # TODO raise proper exception
 
     return resp.text
 
 
-def get_souped_search_results(soup):
+def _get_souped_search_results(soup):
     return soup.find_all("li", class_="p-articles-list-item")
 
 
-def process_soup_list_item(soup_list_item):
+def _process_soup_list_item(soup_list_item):
     url = soup_list_item.find("div", class_="p-item-content-info").find("a")["href"]
     return {
         "id": _extract_id(soup_list_item.find("h2", "p-item-title").a["href"]),
         "price": _extract_price(soup_list_item.find("div", class_="p-item-most-important").string),
         "url": url,
-        "is_finished": is_sale_finished(url)
+        "is_finished": _is_sale_finished(url)
     }
 
 
-def run(current_page_num=1):
+def _run(search_term, current_page_num=1):
     logger.info(f'Searching page {current_page_num}')
-    full_html_resp = get_search_html_response(current_page_num)
+    full_html_resp = _get_search_html_response(current_page_num)
     soup = BeautifulSoup(full_html_resp, "html.parser")
-    soup_results = get_souped_search_results(soup)
-    listed_items = [process_soup_list_item(soup_result) for soup_result in soup_results]
+    soup_results = _get_souped_search_results(soup)
+    listed_items = [_process_soup_list_item(soup_result) for soup_result in soup_results]
     if (_search_has_next_page(soup)
             and not _page_contains_finished_ads(listed_items)):
-        listed_items += run(current_page_num + 1)
+        listed_items += _run(search_term, current_page_num + 1)
 
     return listed_items
+
+
+def check(search_term):
+    """Check Jmty for the given `search_term`. Returns a list of article IDs, prices, etc."""
+    return _run(search_term)
--- src/test/unit/checker/test_jmty.py
+++ src/test/unit/checker/test_jmty.py
@@ -1,6 +1,9 @@
 import unittest
+import unittest.mock as mock
 import main.checker.jmty as jmty_checker
 import os.path
+import re
+
 from bs4 import BeautifulSoup
 
 
@@ -9,7 +12,7 @@ class TestSearchHasNextPage(unittest.TestCase):
     def _get_soup_of_local_file(self, filename):
         this_files_dir = os.path.dirname(os.path.abspath(__file__))
         test_html_filepath = os.path.join(this_files_dir, filename)
-        with open(test_html_filepath) as f:
+        with open(test_html_filepath, encoding='utf-8') as f:
             return BeautifulSoup(f, 'html.parser')
 
     def test_when_only_one_page_results_then_false(self):
@@ -32,22 +35,22 @@ class TestSearchHasNextPage(unittest.TestCase):
 class TestPageContainsFinishedAds(unittest.TestCase):
 
     def _create_unfinished_advert(self):
-        return { "id": 'mock_id',
-            "price": 100,
-            "url": 'mock_url',
-            "is_finished": False
-        }
+        return {"id": 'mock_id',
+                "price": 100,
+                "url": 'mock_url',
+                "is_finished": False
+                }
 
     def _create_finished_advert(self):
-        return { "id": 'mock_id',
-            "price": 100,
-            "url": 'mock_url',
-            "is_finished": True
-        }
+        return {"id": 'mock_id',
+                "price": 100,
+                "url": 'mock_url',
+                "is_finished": True
+                }
 
     def test_when_one_finished_ad_then_true(self):
         mock_adverts = [
-            self._create_unfinished_advert(), 
+            self._create_unfinished_advert(),
             self._create_unfinished_advert(),
             self._create_unfinished_advert(),
             self._create_finished_advert()
@@ -57,7 +60,7 @@ class TestPageContainsFinishedAds(unittest.TestCase):
 
     def test_when_no_finished_ads_then_false(self):
         mock_adverts = [
-            self._create_unfinished_advert(), 
+            self._create_unfinished_advert(),
             self._create_unfinished_advert(),
             self._create_unfinished_advert(),
             self._create_unfinished_advert()
@@ -68,3 +71,54 @@ class TestPageContainsFinishedAds(unittest.TestCase):
     def test_when_no_ads_then_false(self):
         mock_adverts = []
         self.assertFalse(jmty_checker._page_contains_finished_ads(mock_adverts))
+
+
+@mock.patch('main.checker.jmty.requests')
+class TestCheck(unittest.TestCase):
+
+    _finished_sales_ids = ['m5q5d']
+
+    def _assert_number_finished_sales(self, items, number_finished):
+        filtered_items = list(filter(lambda item: item['is_finished'] is True, items))
+        self.assertEqual(len(filtered_items), number_finished)
+
+    def _assert_number_unfinished_sales(self, items, number_unfinished):
+        filtered_items = list(filter(lambda item: item['is_finished'] is False, items))
+        self.assertEqual(len(filtered_items), number_unfinished)
+
+    def _assert_contains_ids(self, items, ids):
+        filtered_items = set([x['id'] for x in filter(lambda item: item['id'] in ids, items)])
+        self.assertEqual(len(filtered_items), len(ids))
+
+    def _file_as_string(self, filepath):
+        this_files_dir = os.path.dirname(os.path.abspath(__file__))
+        test_html_filepath = os.path.join(this_files_dir, filepath)
+        with open(test_html_filepath, encoding='utf-8') as f:
+            return f.read().replace('\n', ' ')
+
+    def _create_mock_response(self, url, **kwargs):
+        # TODO tidy this up
+        if '/sale-' in url and '/article-' in url: # actual article is being requested
+            id_ = re.search('.*/article-(.*)', url).group(1)
+            if id_ in TestCheck._finished_sales_ids:
+                return MockResponse(200, self._file_as_string('mock_finished_sale_article.html'))
+            else: # assume it is an unfinished sale
+                return MockResponse(200, self._file_as_string('mock_unfinished_sale_article.html'))
+        else:
+            return mock.DEFAULT
+
+    def test_when_one_applicable_page_then_correct_objects_returned(self, requests_mock):
+        requests_mock.get.return_value = MockResponse(200, self._file_as_string('results_total_one_page.html'))
+        requests_mock.get.side_effect = self._create_mock_response
+        items = jmty_checker.check('road bike')
+
+        self._assert_number_unfinished_sales(items, 1)
+        self._assert_number_finished_sales(items, 19)
+        self._assert_contains_ids(items, ['m5q5d', 'm86ld'])
+
+
+class MockResponse:
+    """Mock response object from the requests module"""
+    def __init__(self, status_code, text):
+        self.status_code = status_code
+        self.text = text
